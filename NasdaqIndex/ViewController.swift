d11f59bbf4411c661ac424fb45bd421c58dca354
RenchiLiu <raylrc@gmail.com>
2021-05-08 16:01:06 -0500

    init commit

---
+++ NasdaqIndex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NasdaqIndex
+//
+//  Created by liurenchi on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
